442e4269150d149c2fa9781bd8017a3a40a756b4
Tajbid Choudhury <tajbidhussain@hotmail.com>
2017-12-23 01:28:21 -0500

    Created Scripted and Test for NewEgg.ca

---
+++ pyscript.py
@@ -0,0 +1,43 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+my_url = "https://www.newegg.ca/Desktop-Graphics-Cards/SubCategory/ID-48?Tid=7708"
+
+#opening up connection, grabbing the page
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
+
+#html parsing
+page_soup = soup(page_html, "html.parser")
+#page_soup.h1 <- will display everything h1
+
+#grabs each product
+containers = page_soup.findAll("div", {"class":"item-container"})
+
+#find length with -> len(containers)
+#display first index from findall -> container[0]
+
+filename = "products.csv"
+f = open(filename, "w")
+headers = "brand, product_name, shipping \n"
+
+f.write(headers)
+
+for container in containers:
+    brand = container.div.div.a.img["title"]
+
+    title_container = container.findAll("a", {"class":"item-title"})
+    product_name  = title_container[0].text
+
+    shipping_container = container.findAll("li", {"class":"price-ship"})
+    shipping = shipping_container[0].text.strip()
+
+
+    print("brand: " + brand)
+    print("product_name: " + product_name)
+    print ("shipping: " + shipping)
+
+    f.write(brand + "," + product_name.replace(",","|") + "," + shipping + "\n")
+    
+f.close()
